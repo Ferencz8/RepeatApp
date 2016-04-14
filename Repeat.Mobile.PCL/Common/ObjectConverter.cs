c13a5e9351e3fea22cc5d410c45e9e4326dae57c
Ferencz Veres <ferenczv8@gmail.com>
2016-04-14 14:33:42 +0300

    commit ObjectConverter class

---
+++ Repeat.Mobile.PCL/Common/ObjectConverter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat.Mobile.PCL.Common
+{
+	public static class ObjectConverter
+	{
+
+		private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings()
+		{
+			ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+		};
+
+		public static string ToJSON<T>(T obj)
+			where T : class
+		{
+			return JsonConvert.SerializeObject(obj, _settings);
+		}
+
+		public static T ToObject<T>(string jsonStringOfObject)
+			where T : class
+		{
+			return JsonConvert.DeserializeObject<T>(jsonStringOfObject, _settings);
+		}
+	}
+}
